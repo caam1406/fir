f0e5065ab159dddd69a6cd402c7f9e6f28b91360
Carlos Matos <caam1406@gmail.com>
2022-04-28 12:41:00 -0300

    feat: added layer from controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './modules/todo/todo.module';
 import { TodoService } from './services/todo/todo.service';
+import { TodoController } from './controllers/todo/todo.controller';
 
 @Module({
   imports: [TodoModule],
-  controllers: [AppController],
+  controllers: [AppController, TodoController],
   providers: [AppService, TodoService],
 })
 export class AppModule {}
