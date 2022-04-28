13f80a7304991a80b47555b8c7b3402ce63feda9
Carlos Matos <caam1406@gmail.com>
2022-04-28 13:05:07 -0300

    refactor: added enviroment data in configService model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TodoModule } from './modules/todo/todo.module';
-import { TodoService } from './services/todo/todo.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoController } from './controllers/todo/todo.controller';
+// import { TodoModule } from './modules/todo/todo.module';
+import { TodoService } from './services/todo/todo.service';
 
 @Module({
-  imports: [TodoModule],
-  controllers: [AppController, TodoController],
-  providers: [AppService, TodoService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 3306)),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', '123'),
+        database: configService.get('DB_NAME', 'todo'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
+  controllers: [TodoController],
+  providers: [TodoService],
 })
 export class AppModule {}
