7bd887bd167285483daa089dd073ffc0d3c6ba70
Carlos Matos <caam1406@gmail.com>
2022-04-28 12:38:26 -0300

    feat: added layer from services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './modules/todo/todo.module';
+import { TodoService } from './services/todo/todo.service';
 
 @Module({
   imports: [TodoModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
